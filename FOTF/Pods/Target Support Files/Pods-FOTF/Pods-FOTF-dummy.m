426f0be154b16f68af5f3463dea4e0e0c41cebf6
Sabrina Niklaus <sabrinamarlo@D-69-91-143-19.dhcp4.washington.edu>
2017-05-18 18:04:01 -0700

    added firebase

---
+++ FOTF/Pods/Target Support Files/Pods-FOTF/Pods-FOTF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FOTF : NSObject
+@end
+@implementation PodsDummy_Pods_FOTF
+@end
---
+++ FOTF/Pods/Target Support Files/Pods-FOTFTests/Pods-FOTFTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FOTFTests : NSObject
+@end
+@implementation PodsDummy_Pods_FOTFTests
+@end
