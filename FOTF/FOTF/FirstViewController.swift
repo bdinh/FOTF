a1813dc2c5baa763055002b25868a7f3cbd4a96e
Sabrina Niklaus <sabrinamarlo@D-69-91-143-19.dhcp4.washington.edu>
2017-05-18 17:06:56 -0700

    initialized xcode project

---
+++ FOTF/FOTF/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FOTF
+//
+//  Created by Sabrina Niklaus on 5/18/17.
+//  Copyright © 2017 info-449. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
