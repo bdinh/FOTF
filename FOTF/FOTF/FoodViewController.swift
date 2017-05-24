3e46f2121195625f18785226f41eb4b7035660bf
Sabrina Niklaus <sabrinamarlo@D-108-179-144-107.dhcp4.washington.edu>
2017-05-24 15:35:45 -0700

    added api request code

--- FOTF/FOTF/FoodViewController.swift
+++ FOTF/FOTF/FoodViewController.swift
@@ -10,6 +10,65 @@ import UIKit
 
 class FoodViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
+    var jsonResponse: NSDictionary!
+    let apiID: String = "d09f36f1"
+    let apiKey: String = "fc4200e582f9de3d0b19ef0716196032"
+    // not connected to anything
+    @IBOutlet weak var searchField: UITextField!
+    
+    // not connected to anything 
+    @IBAction func searchClicked(_ sender: UIButton) {
+        let searchString = searchField.text!
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.nutritionix.com/v1_1/search/")! as URL)
+        let session = URLSession.shared
+        request.httpMethod = "POST"
+        
+        let params = [
+            "appId" : apiID,
+            "appKey" : apiKey,
+            "fields" : ["item_name", "brand_name", "keywords", "usda_fields"],
+            "limit" : "50",
+            "query" : searchString,
+            "filters" : ["exists" :["usda_fields": true]]] as [String : Any]
+        
+        var _: NSError?
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params)
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+        } catch {
+            print(error)
+        }
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, err) -> Void in
+            var conversionError: NSError?
+            
+            do {
+                let jsonDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableLeaves) as? NSDictionary
+                print(jsonDictionary!)
+                if conversionError != nil {
+                    print(conversionError!.localizedDescription)
+                    let errorString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                    print("Error in Parsing \(String(describing: errorString))")
+                }
+                else {
+                    if jsonDictionary != nil {
+                        self.jsonResponse = jsonDictionary!
+                        print(self.jsonResponse)
+                    }
+                    else {
+                        let errorString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                        print("Error Could not Prase JSON \(String(describing: errorString))")
+                    }
+                }
+                
+            } catch {
+                print(conversionError)
+            }
+            
+        })
+        task.resume()
+    }
+
     func numberOfSections(in tableView: UITableView) -> Int {
         // will have it based on dates
         return 1
