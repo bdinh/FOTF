3f4d6bd1858bec4269bcaa7ac0f1e71515fe1109
Sabrina Niklaus <sabrinamarlo@Sabrinas-MBP.hsd1.wa.comcast.net>
2017-05-26 10:31:09 -0700

    created food class, upon api request food items are created and stored in an array. Added alamo fire

--- FOTF/FOTF/FoodViewController.swift
+++ FOTF/FOTF/FoodViewController.swift
@@ -7,24 +7,17 @@
 //
 
 import UIKit
+import Alamofire
 
 class FoodViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
-    var jsonResponse: NSDictionary!
+    var jsonResponse: NSDictionary = [:]
+    var searchResults: [Food] = []//[String: [String:Any]] = [:]
     let apiID: String = "d09f36f1"
     let apiKey: String = "fc4200e582f9de3d0b19ef0716196032"
     
-    // not connected to anything
-    @IBOutlet weak var searchField: UITextField!
-    
-    // not connected to anything 
-    @IBAction func searchClicked(_ sender: UIButton) {
-        let query = searchField.text!
-        let request = NSMutableURLRequest(url: NSURL(string: "https://api.nutritionix.com/v1_1/search/")! as URL)
-        let session = URLSession.shared
-        request.httpMethod = "POST"
-        
-        let params = [
+    func attemptRequest(query: String) {
+        let parameters = [
             "appId" : apiID,
             "appKey" : apiKey,
             "fields" : ["item_name", "brand_name", "keywords", "usda_fields"],
@@ -32,42 +25,53 @@ class FoodViewController: UIViewController, UITableViewDataSource, UITableViewDe
             "query" : query,
             "filters" : ["exists" :["usda_fields": true]]] as [String : Any]
         
-        var _: NSError?
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: params)
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-        } catch {
-            print(error)
-        }
-        let task = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, err) -> Void in
-            var conversionError: NSError?
-            
-            do {
-                let jsonDictionary = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableLeaves) as? NSDictionary
-                print(jsonDictionary!)
-                if conversionError != nil {
-                    print(conversionError!.localizedDescription)
-                    let errorString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                    print("Error in Parsing \(String(describing: errorString))")
-                }
-                else {
-                    if jsonDictionary != nil {
-                        self.jsonResponse = jsonDictionary!
-                        print(self.jsonResponse)
-                    }
-                    else {
-                        let errorString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-                        print("Error Could not Prase JSON \(String(describing: errorString))")
-                    }
-                }
-                
-            } catch {
-                print(conversionError)
+        Alamofire.request("https://api.nutritionix.com/v1_1/search/", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                self.processResponse(response: response)
+//                self.tableView.reloadData()
+            case .failure(let error):
+                print(error)
             }
-            
-        })
-        task.resume()
+        }
+    }
+    
+    func processResponse(response: DataResponse<Any>) {
+        print("got here")
+        let dictionary = response.result.value as! NSDictionary
+        
+        let hits = dictionary["hits"] as! [[String:Any]]
+        for hit in hits {
+            let info = hit["fields"] as! [String:Any]
+            let name = info["item_name"] as! String
+            let usda = info["usda_fields"] as! [String:Any]
+            let foodItem = Food()
+            foodItem.title = name
+            foodItem.usda_fields = usda
+            searchResults.append(foodItem)
+   
+//            let calories = usda["ENERC_KCAL"] as! [String:String]
+//            foodItem.calories = "\(calories["value"]) \(calories["uom"])"
+//            let fat = usda["FAMS"] as! [String:String]
+//            foodItem.fat = "\(fat["value"]) \(fat["uom"])"
+//            let cholestrol = usda["CHOLE"] as! [String:String]
+//            foodItem.cholestrol = "\(cholestrol["value"]) \(cholestrol["uom"])"
+//            let sodium = usda["NA"] as! [String:String]
+//            foodItem.sodium = "\(sodium["value"]) \(sodium["uom"])"
+//            let potassium = usda["K"] as! [String:String]
+//            foodItem.potassium = "\(potassium["value"]) \(potassium["uom"])"
+//            let fiber = usda["FIBTG"] as! [String:String]
+//            foodItem.fiber = "\(fiber["value"]) \(fiber["uom"])"
+//            let sugar = usda["SUGAR"]as! [String:String]
+//            foodItem.sugar = "\(sugar["value"]) \(sugar["uom"])"
+//            let protein = usda["PROCNT"] as! [String:String]
+//            foodItem.protein = "\(protein["value"]) \(protein["uom"])"
+//            
+//            self.searchResults.append(foodItem)
+//        }
+//        print("search results: \(searchResults)") 
+        }
+        print(self.searchResults)
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -86,6 +90,7 @@ class FoodViewController: UIViewController, UITableViewDataSource, UITableViewDe
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.attemptRequest(query: "apple")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
