8321d5daef74fd1a4dbeb63accb227b3ff0b4e55
bdinh <baodinh96@gmail.com>
2017-06-06 18:08:30 -0700

    Error handling

--- FOTF/FOTF/ComposeNewFoodEntryViewController.swift
+++ FOTF/FOTF/ComposeNewFoodEntryViewController.swift
@@ -145,10 +145,14 @@ class ComposeNewFoodEntryViewController: UIViewController, UITableViewDataSource
         return cell
         
     }
-    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
         tableView.delegate = self
         tableView.dataSource = self
         ref = Database.database().reference()
